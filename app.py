4e26d32fe0e419573b81a539da1554b82c22335f
AltTether <200351p@ugs.kochi-tech.ac.jp>
2018-12-31 01:34:58 +0900

    カメラからArUcoマーカを読み取ってレスポンスとして返すように変更

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
+import cv2
+import base64
+
 from flask import Flask
+from src import Camera, ArUco, ndarray_ids_to_list
+
+
+CAMERA_PORT = 0
+
 app = Flask(__name__)
+cam = Camera(CAMERA_PORT)
+arc = ArUco()
 
 @app.route('/')
 def main():
-    return "Hello"
+    img = cam.capturing()
+    _, ids, _ = arc.detecting(img)
+
+    response = None
+    if ids == None:
+        response = 'cannot detecting aruco marker'
+    else:
+        ids = ndarray_ids_to_list(ids)
+        response = ' '.join([str(id_) for id_ in ids])
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
